b0e9d90a00847c7f4a60c97f3f245b9932ef074f
TiagoMasaro <137321268+TiagoMasaro@users.noreply.github.com>
2026-08-26 19:23:46 -0300

    Commit Atividade 26 08

---
+++ Atividade6/Exercicio9.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Exercicio9
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a fita de DNA: (lembrando que as fitas são compostas por A, C, T, G)");
+        string fita = Console.ReadLine().ToUpper();
+
+        char[] dnaArray = fita.ToCharArray();
+        char[] complementar = new char[dnaArray.Length];
+
+        for (int i = 0; i < dnaArray.Length; i++)
+        { //comeco do for
+            char dna = dnaArray[i];
+
+            if (dna == 'A')
+            {
+                complementar[i] = 'T';
+            }
+            else if (dna == 'T')
+            {
+                complementar[i] = 'A';
+            }
+            else if (dna == 'C')
+            {
+                complementar[i] = 'G';
+            }
+            else if (dna == 'G')
+            {
+                complementar[i] = 'C';
+            }
+            else
+            {
+                Console.WriteLine("Base inválida encontrada: " + dna);
+                return;
+            }
+        } //fim do for
+
+        string fitaComplementar = new string(complementar);
+
+        Console.WriteLine("Fita original: " + fita);
+        Console.WriteLine("Fita complementar: " + fitaComplementar);
+    }
+}
+
+
+
