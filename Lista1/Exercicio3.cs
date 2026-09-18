606e8b2518bff08708bceb163451efd612aec9fe
TiagoMasaro <137321268+TiagoMasaro@users.noreply.github.com>
2026-09-17 23:49:31 -0300

    Commit Lista1

---
+++ Lista1/Exercicio3.cs
@@ -0,0 +1,27 @@
+using System;
+using BibliotecaFuncoes;
+ class Exercicio3 {
+    
+    static double MaiorNumero(double[] vetor) {
+        double maior = vetor[0];
+        for(int i = 0; i < vetor.Length; i++) {
+            if(maior < vetor[i])
+                maior = vetor[i];
+        }
+        return maior;
+    }
+    static void Main() {
+        int n;
+        double maior;
+        Console.WriteLine("Qual o tamanho do vetor: ");
+        n = int.Parse(Console.ReadLine());
+        double [] vetor = new double[n];
+        Biblioteca.GerarVetorDouble(vetor);
+        Biblioteca.MostrarVetorDouble(vetor);
+        maior = MaiorNumero(vetor);
+        Console.WriteLine($"Maior valor do vetor: {maior:f2}.");
+
+
+    }
+}
+
